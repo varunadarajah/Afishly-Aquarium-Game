de5d829807938fd923cc23adc0720603ea063787
varun <varunadarajah@gmail.com>
2023-04-15 17:50:22 -0700

    small fish class changes

--- Assets/Scripts/Fish.cs
+++ Assets/Scripts/Fish.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class Fish : MonoBehaviour
 {
+    public string fishBreed;
     public string fishName;
+    public string dateObtained;
+    public Color fishColor;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        dateObtained = System.DateTime.UtcNow.ToLocalTime().ToString("MM/dd/yyyy");
     }
 
     // Update is called once per frame
