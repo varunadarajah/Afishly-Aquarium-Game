7b3b27d12a65742b58217d6e3ce7d297fd1077ec
varun <varunadarajah@gmail.com>
2023-04-20 19:22:02 -0700

    implemented fish color functionality

--- Assets/Scripts/Fish.cs
+++ Assets/Scripts/Fish.cs
@@ -9,17 +9,21 @@ public class Fish : MonoBehaviour
     public string dateObtained;
     public Color fishColor;
 
+    SpriteRenderer colorSprite;
+
     public int rarity;
 
     // Start is called before the first frame update
     void Start()
     {
         dateObtained = System.DateTime.UtcNow.ToLocalTime().ToString("MM/dd/yyyy");
+        fishColor = Color.white;
+        colorSprite = GetComponentsInChildren<SpriteRenderer>()[1]; // gets silloute sprite
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        colorSprite.color = fishColor;
     }
 }
